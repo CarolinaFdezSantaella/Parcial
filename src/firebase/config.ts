0f8f6cd11a1e5ff3a015e25f93eb7ba1d0b0aa6d
Carolina Fernandezdesantaella <carolinafsantaella@gmail.com>
2025-10-28 19:47:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5384099739-efe01",
+  "appId": "1:653971486416:web:d4da0db34081fc0bd310d5",
+  "apiKey": "AIzaSyACpHo7oke3HgyATyS1vMAK6w2bUX9tBq4",
+  "authDomain": "studio-5384099739-efe01.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "653971486416"
+};
